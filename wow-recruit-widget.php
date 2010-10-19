36d01cf4dcd8b6ef7c97a359dd58f190b05a6c44
freegod725 <freegod725@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-19 15:28:57 +0000

    git-svn-id: http://plugins.svn.wordpress.org/wow-recruit-widget/trunk@301748 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wow-recruit-widget.php
+++ wow-recruit-widget.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WOW Recruitment Widget
  * Plugin URI: http://www.ycfreeman.com/p/wow-recruitment-wordpress-widget.html
- * Description: A widget that helps to display recruitment message of a World of Warcraft guild.
+ * Description: A widget that helps to display recruitment message of a World of Warcraft guild, also can be used for other games that has different classes.
  * please save the widget once after upgrade from 1.0.x to make it work with new codes, 
  * make sure you backup those color codes before upgrade if you have changed them before
  * Version: 1.3.1
